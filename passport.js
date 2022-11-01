a346a3eca2de0a9f58c821ae3315ea5cfe623c43
Minhaj Islam <minhajislam674@gmail.com>
2022-11-01 17:01:25 +0100

    Installed passport and configured local and JWT passport strategies

---
+++ passport.js
@@ -0,0 +1,43 @@
+const passport = require('passport'),
+    LocalStrategy = require('passport-local').Strategy, //two Passport strategies are defined: LocalStrategy and JWTStrategy 
+    Models = reqzure('./models.js'),
+    passportJWT = require('passport-jwt');
+
+let Users = Models.User,
+    JWTStrategy = passportJWT.Strategy,
+    ExtractJWT = passportJWT.ExtractJWT;
+
+passport.use(new LocalStrategy ({ //LocalStrategy takes a username and password from the request body
+    usernameField: 'Username',
+    passwordField: 'Password'
+}, 
+(username, password, callback) => {
+    console.log(username + '  ' + password);
+    Users.findOne({Username: username}) //uses Mongoose to check our database for a user with the same username
+    .then((user) => {
+        if (!user) {
+            console.log('incorrect username');
+            return callback(null, false, {message: 'Incorrect username or passowrd.'});
+        }
+        console.log('finished');
+        return callback(null, user, {message: 'Logged In Successfully'});
+    })
+    .catch((error) => {
+        console.log(error);
+        return callback(error)
+    });
+}));
+
+//Setting up the JWT authentication code
+passport.use(new JWTStrategy ({
+    jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(), //JWT is extracted from the header of the HTTP request. This JWT is called the “bearer token”.
+    secretOrKey: 'your_jwt_secret' //Use of a “secret” key to verify the signature of the JWT.
+}, (jwtPayload, callback) => {
+    return Users.findById(jwtPayload._id)
+    .then((user) => {
+        return callback(null, user);
+    })
+    .catch((error) => {
+        return callback(error);
+    });
+}));
